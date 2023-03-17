bb96dcee550e527191028b0de4e057e6fce5ff24
Moothes <zhouhuajun123@126.com>
2023-03-17 17:54:11 +0800

    Delete fps.sh

--- fps.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-#for i in dhsnet amulet nldf srm picanet dss basnet cpd poolnet egnet scrn gcpa itsd minet
-#for i in f3net ldf gatenet pfsnet ctdnet
-for i in dhsnet itsd scrn
-do python3 test_fps.py $i --gpus=$1 --backbone=vgg
-python3 test_fps.py $i --gpus=$1
-done
