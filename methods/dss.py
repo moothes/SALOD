d559e3b4d26916a8c7cecc96662e2ad287c769ae
Moothes <zhouhuajun123@126.com>
2024-10-11 10:31:09 +0800

    Update dss.py

--- methods/dss.py
+++ methods/dss.py
@@ -4,7 +4,7 @@ from torch.nn import init
 
 
 
-custom_config = {'base'      : {'strategy': 'base_adam',
+custom_config = {'base'      : {'strategy': 'adam_base',
                                 'batch': 8,
                                },
                  'customized': {'--abc': {'type': float, 'default': 0},
