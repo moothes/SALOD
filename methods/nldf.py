19e4102989026fb789e3120d5d1cbe2d8561d4c2
Moothes <zhouhuajun123@126.com>
2024-10-11 10:34:13 +0800

    Update nldf.py

--- methods/nldf.py
+++ methods/nldf.py
@@ -3,7 +3,7 @@ from torch import nn
 import torch.nn.functional as F
 
 
-custom_config = {'base'      : {'strategy': 'base_adam',
+custom_config = {'base'      : {'strategy': 'adam_base',
                                 'batch': 8,
                                },
                  'customized': {'--abc': {'type': float, 'default': 0},
