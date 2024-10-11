f273444e9a40ba65268916e2c0baf10350e6819b
Moothes <zhouhuajun123@126.com>
2024-10-11 10:31:52 +0800

    Update edn.py

--- methods/edn.py
+++ methods/edn.py
@@ -4,7 +4,7 @@ from torch import nn
 from torch.nn import functional as F
 
 
-custom_config = {'base'      : {'strategy': 'base_adam',
+custom_config = {'base'      : {'strategy': 'adam_base',
                                 'batch': 8,
                                },
                  'customized': {'--abc': {'type': float, 'default': 0},
