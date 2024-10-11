1e4c663eff496c587325ea5e4a30d87bfac3725c
Moothes <zhouhuajun123@126.com>
2024-10-11 10:32:04 +0800

    Update egnet.py

--- methods/egnet.py
+++ methods/egnet.py
@@ -7,7 +7,7 @@ from torch.autograd import Variable
 import numpy as np
 
 
-custom_config = {'base'      : {'strategy': 'base_adam',
+custom_config = {'base'      : {'strategy': 'adam_base',
                                 'batch': 8,
                                },
                  'customized': {'--abc': {'type': float, 'default': 0},
