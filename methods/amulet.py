21313b059f3f690ac21418be40520e0a587d49bb
Moothes <zhouhuajun123@126.com>
2024-10-11 10:29:11 +0800

    Update amulet.py

--- methods/amulet.py
+++ methods/amulet.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 
-custom_config = {'base'      : {'strategy': 'base_adam',
+custom_config = {'base'      : {'strategy': 'adam_base',
                                 'batch': 8,
                                },
                  'customized': {'--abc': {'type': float, 'default': 0},
